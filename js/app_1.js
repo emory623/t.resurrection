886ecc3fc766a410516de4443609da9c08ca1bd8
emory623 <dvanullnulldva@gmail.com>
2022-05-04 02:45:50 +0300

    `2ndPractice`

--- js/app_1.js
+++ js/app_1.js
@@ -1,7 +1,13 @@
 `use strict`;
 
-const numberOfFilms = +prompt('Сколько фильмов ты уже посмотрел?'); //1
+let numberOfFilms;
 
+for(let i = 0; i < 1; i++){
+    numberOfFilms = +prompt('Сколько фильмов ты уже посмотрел?'); //1
+    if(numberOfFilms == false || numberOfFilms == '' || numberOfFilms.length > 50){
+        --i;
+    }
+}
 const personalMovieDB = { //2
     count: numberOfFilms,
     movies: {},
@@ -10,11 +16,24 @@ const personalMovieDB = { //2
     private: false
 }
 
-for(let i = 0; i < 2; i++){
+for(let i = 0; i < 3; i++){
     const lastMovie = prompt('Один из последних фильмов'), // 3
             markMovie = +prompt('насколько оценишь7');
-    
-    personalMovieDB.movies[lastMovie] = markMovie;
+    if(lastMovie == false || lastMovie == '' || lastMovie >50 || markMovie == false || markMovie == '' || markMovie > 50){
+        --i;
+    }else{
+        personalMovieDB.movies[lastMovie] = markMovie;
+    }
 }
 
 console.log(personalMovieDB.movies)
+
+// practice 2
+
+if(personalMovieDB.count < 10){
+    console.log('malo');
+}else if(personalMovieDB.count > 10){
+    console.log('normal');
+}else if(personalMovieDB.count > 30){
+    console.log('wow');
+}
