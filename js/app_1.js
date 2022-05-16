2ff9b7f29cd19971a86dba58560b025a727c2174
emory623 <dvanullnulldva@gmail.com>
2022-05-16 17:08:46 +0300

    'functionPractice'

--- js/app_1.js
+++ js/app_1.js
@@ -1,39 +1,71 @@
-// `use strict`;
+`use strict`;
 
-// let numberOfFilms;
+let numberOfFilms;
 
-// for(let i = 0; i < 1; i++){
-//     numberOfFilms = +prompt('Сколько фильмов ты уже посмотрел?'); //1
-//     if(numberOfFilms == false || numberOfFilms == '' || numberOfFilms.length > 50){
-//         --i;
-//     }
-// }
-// const personalMovieDB = { //2
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     private: false
-// }
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов ты уже посмотрел?');
+    while(numberOfFilms == false || numberOfFilms == '' || numberOfFilms.length > 50 || isNaN(numberOfFilms)){
+            numberOfFilms = +prompt('Сколько фильмов ты уже посмотрел?');
+    }
+}
 
-// for(let i = 0; i < 3; i++){
-//     const lastMovie = prompt('Один из последних фильмов'), // 3
-//             markMovie = +prompt('насколько оценишь7');
-//     if(lastMovie == false || lastMovie == '' || lastMovie >50 || markMovie == false || markMovie == '' || markMovie > 50){
-//         --i;
-//     }else{
-//         personalMovieDB.movies[lastMovie] = markMovie;
-//     }
-// }
+// start();
+
+const personalMovieDB = { //2
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+}
+
+function rememberMyFilms(){
+    for(let i = 0; i < 3; i++){
+        const lastMovie = prompt('Один из последних фильмов'), // 3
+                markMovie = +prompt('насколько оценишь7');
+        if(lastMovie == false || lastMovie == '' || lastMovie >50 || markMovie == false || markMovie == '' || markMovie > 50){
+            --i;
+        }else{
+            personalMovieDB.movies[lastMovie] = markMovie;
+        }
+    }
+}
 
 // console.log(personalMovieDB.movies)
+// rememberMyFilms();
 
-// // practice 2
+// practice 2
 
-// if(personalMovieDB.count < 10){
-//     console.log('malo');
-// }else if(personalMovieDB.count > 10){
-//     console.log('normal');
-// }else if(personalMovieDB.count > 30){
-//     console.log('wow');
-// }
+function detectPersonalLvl(){
+    if(personalMovieDB.count < 10){
+        console.log('malo');
+    }else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log('normal');
+    }else if(personalMovieDB.count > 30){
+        console.log('wow');
+    }
+}
+
+detectPersonalLvl();
+
+function showMyDB(){
+    if(personalMovieDB.private == false){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres(){
+    for(let i = 0; i < 3; i++){
+        const count = prompt(`write your favorite genre #${i+1}`)
+        if(count == false || count == '' || typeof(count) == 'number' || count == null){
+            --i;
+        }else{
+            personalMovieDB.genres[i] = count;
+        }
+    }
+}
+
+writeYourGenres();
+console.log(personalMovieDB.genres);
